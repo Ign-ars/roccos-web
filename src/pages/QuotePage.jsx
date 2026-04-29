4004797b17bfa8d5a3ddf65b8338894c0cfdd649
Ignacio Aros <ignacio.aros.seguel@gmail.com>
2026-04-29 13:01:24 -0400

    conecta backend render

--- src/pages/QuotePage.jsx
+++ src/pages/QuotePage.jsx
@@ -55,7 +55,7 @@ export default function QuotePage() {
     try {
       setLoading(true);
 
-      const response = await fetch("http://localhost:4000/api/cotizacion", {
+      const response = await fetch("https://roccos-backend.onrender.com/api/cotizacion", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
